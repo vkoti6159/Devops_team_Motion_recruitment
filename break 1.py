619518dd0203bd3d19db925be851b6f6a76d9a5d
Varikuti Koteswara rao <22ht1a6159@gmail.com>
2025-02-24 12:24:48 +0530

    Add files via upload

---
+++ break 1.py
@@ -0,0 +1,12 @@
+#using while loop
+i=1
+while i<=5:
+    print(i)
+    i+=1
+    if(i==4):
+        break
+ # using for loop   
+for i in range(1,6):
+    if (i==4):
+        break
+    print(i)    
