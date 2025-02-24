d382ddd99335537e1119fc15b423f2793edbb2ac
Varikuti Koteswara rao <22ht1a6159@gmail.com>
2025-02-24 12:31:05 +0530

    Add files via upload

---
+++ Asssignment.py
@@ -0,0 +1,16 @@
+'''
+Assignment oprarators :  it is used to assign the right fand side values to left hand side variables 
+Assignment operators : =, +=,-=,*=,/=,%=
+'''
+a=2
+print(a)
+a+=5       #which means a=a+5
+print(a)
+a-=1
+print(a)
+a*=2
+print(a)
+a/=2
+print(a)
+a%=1      #stores the remainder into  left hand side variable
+print(a)
